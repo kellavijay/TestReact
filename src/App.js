96d42017160302925b1d1c9770c9cf9ac33c4515
vijayakumar.kella <vijayakumar.kella@spsoftglobal.com>
2024-02-21 11:21:31 +0530

    updated appjs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and savdddddddddde to reloadd.
+          Edit <code>src/App.js</code> Vijay.
         </p>
         <a
           className="App-link"
